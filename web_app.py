2316b6418f4c7caa35e116ac5d09ae9f4011afdf
prudhviraju0108 <prudhviraju0108@gmail.com>
2020-02-17 07:57:25 -0700

    Passwordvault module

--- web_app.py
+++ web_app.py
@@ -1,18 +1,13 @@
 from flask import Flask, render_template,request
-import mysql.connector
 import yaml
 from flask_mysqldb import MySQL
-
-
 app = Flask(__name__)
-
 db = yaml.load(open("db.yaml"))
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
 mysql = MySQL(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -21,12 +16,19 @@ def index():
         name = userdetails['name']
         user_name = userdetails['user_name']
         password = userdetails['password']
-        cursor = mysql.connecton.cursor
-        cursor.execute("INSERT into details(name,user_name,password) VALUES(%s, %s, %s)",(name,user_name,password))
-        mysql.connection.commit
-        cursor.close()
-        return "success"
+        cursor = mysql.connection.cursor()
+        try:
+            cursor.execute("INSERT into details(name,user_name,password) VALUES("'"{0}"'", "'"{1}"'", "'"{2}"'")".format(name,user_name,password))
+            mysql.connection.commit()
+            cursor.close()
+            return "success"
+        except:
+            return "fail"
     return render_template('index.html')
+
+@app.route('/dtls', methods=['GET'])
+def dtls():
+    
 
 
 app.run()
